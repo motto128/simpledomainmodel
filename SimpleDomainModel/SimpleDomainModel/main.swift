bd4a955a95295c9b94aa443a338dd955f78598bd
Joe Motto <joermotto@gmail.com>
2018-01-29 15:50:52 -0800

    fixed problem

--- SimpleDomainModel/SimpleDomainModel/main.swift
+++ SimpleDomainModel/SimpleDomainModel/main.swift
@@ -28,11 +28,57 @@ public struct Money {
   public var currency : String
   
   public func convert(_ to: String) -> Money {
+    switch to {
+    case "USD":
+        if (self.currency == "GBP"){
+            return Money(amount: Int(self.amount * 2), currency: "USD")
+        }else if (self.currency == "CAN"){
+            return Money(amount: Int(self.amount * 4/5), currency: "USD")
+        }else if(self.currency == "EUR"){
+            return Money(amount: Int(self.amount * 2/3), currency: "USD")
+        }else{
+            return self;
+        }
+    case "EUR":
+            if (self.currency == "USD"){
+                return Money(amount: Int(self.amount * 3/2), currency: "EUR")
+            }else if (self.currency == "GBP"){
+                return Money(amount: Int(self.amount * 3), currency: "EUR")
+            }else if(self.currency == "CAN"){
+                return Money(amount: Int(self.amount * 6/5), currency: "EUR")
+            }else{
+                return self;
+        }
+    case "CAN":
+        if (self.currency == "USD"){
+            return Money(amount: Int(self.amount * 5/4), currency: "CAN")
+        }else if (self.currency == "GBP"){
+            return Money(amount: Int(self.amount * 5/2), currency: "CAN")
+        }else if(self.currency == "EUR"){
+            return Money(amount: Int(self.amount * 5/6), currency: "CAN")
+        }else{
+            return self;
+        }
+    case "GBP":
+        if (self.currency == "USD"){
+            return Money(amount: Int(self.amount * 1/2), currency: "GBP")
+        }else if (self.currency == "CAN"){
+            return Money(amount: Int(self.amount * 2/5), currency: "GBP")
+        }else if(self.currency == "EUR"){
+            return Money(amount: Int(self.amount * 1/3), currency: "GBP")
+        }else{
+            return self;
+        }
+    default:
+        return self;
+    }
   }
   
   public func add(_ to: Money) -> Money {
+    return Money(amount:self.convert(to.currency).amount + to.amount , currency:to.currency)
   }
   public func subtract(_ from: Money) -> Money {
+    return Money(amount: self.amount - from.convert(self.currency).amount, currency: self.currency)
   }
 }
 
@@ -49,12 +95,26 @@ open class Job {
   }
   
   public init(title : String, type : JobType) {
+    self.title = title
+    self.type = type
   }
   
   open func calculateIncome(_ hours: Int) -> Int {
+    switch self.type {
+    case .Hourly(let val):
+        return Int(val * Double(hours))
+    case .Salary(let val):
+        return val
+    }
   }
   
   open func raise(_ amt : Double) {
+    switch self.type {
+    case .Hourly(let val):
+        self.type = JobType.Hourly(val + amt)
+    case .Salary(let val):
+        self.type = JobType.Salary(val + Int(amt))
+    }
   }
 }
 
@@ -68,15 +128,17 @@ open class Person {
 
   fileprivate var _job : Job? = nil
   open var job : Job? {
-    get { }
+    get { return self._job }
     set(value) {
+        self._job = self.age > 15 ? value : nil
     }
   }
   
   fileprivate var _spouse : Person? = nil
   open var spouse : Person? {
-    get { }
+    get { return self._spouse }
     set(value) {
+        self._spouse = self.age >= 18 ? value: nil
     }
   }
   
@@ -87,6 +149,7 @@ open class Person {
   }
   
   open func toString() -> String {
+    return "[Person: firstName:\(self.firstName) lastName:\(self.lastName) age:\(self.age) job:\(String(describing: self.job)) spouse:\(String(describing: self.spouse))]"
   }
 }
 
@@ -97,13 +160,29 @@ open class Family {
   fileprivate var members : [Person] = []
   
   public init(spouse1: Person, spouse2: Person) {
+    members.append(spouse1)
+    members.append(spouse2)
   }
   
   open func haveChild(_ child: Person) -> Bool {
+    for member in members {
+        if member.age >= 21 {
+            members.append(child)
+            return true
+        }
+    }
+    return false
   }
   
   open func householdIncome() -> Int {
-  }
+    var res: Int = 0
+    for member in members {
+        if let job = member.job {
+            res += job.calculateIncome(2000)
+        }
+    }
+    return res
+    }
 }
 
 
